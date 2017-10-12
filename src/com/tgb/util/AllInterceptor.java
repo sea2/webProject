1564cd48a04c77cd400c7439c5da8e60d85ecb16
sea2 <497661292@qq.com>
2017-10-12 14:02:14 +0800

    支持jsonp的拦截器

--- src/com/tgb/util/AllInterceptor.java
+++ src/com/tgb/util/AllInterceptor.java
@@ -22,16 +22,17 @@ public class AllInterceptor extends HandlerInterceptorAdapter {
 	 * 从最后一个拦截器往回执行所有的postHandle() 接着再从最后一个拦截器往回执行所有的afterCompletion()
 	 */
 	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
 		String requestUri = request.getRequestURI(); // 请求完整路径，可用于登陆后跳转
 		String contextPath = request.getContextPath(); // 项目下完整路径
 		String url = requestUri.substring(contextPath.length()); // 请求页面
 		System.out.print("\n" + TimeUtil.getNowTime() + "发生拦截..." + request.getMethod() + "..." + url);
 		System.out.println("请求：" + request.getRequestURL());
 
-		/*if (new CustomUri().getListStr().contains(requestUri)) {
+		if (new CustomUri().getListStr().contains(requestUri)) {
 			return true;
-		} else {//小财迷测试服务
+		} else {// 小财迷测试服务
 			BufferedReader in = new BufferedReader(new InputStreamReader(request.getInputStream()));
 			StringBuilder sb = new StringBuilder();
 			String line = null;
@@ -40,27 +41,35 @@ public class AllInterceptor extends HandlerInterceptorAdapter {
 			}
 			String urlStr = "http://test.91xcm.com:8020" + requestUri;
 			System.out.println("请求参数：" + sb.toString());
-			String jsonStr = HttpUtil.interceptorHttp(requestUri, urlStr, sb.toString(), request.getMethod(), request.getQueryString());
+			String jsonStr = HttpUtil.interceptorHttp(requestUri, urlStr, sb.toString(), request.getMethod(),
+					request.getQueryString());
 			try {
+				// 正常接口返回
 				response.setContentType("application/json;charset=UTF-8");
 				response.setCharacterEncoding("UTF-8");
 				response.getWriter().write(jsonStr);
 				response.getWriter().flush();
 				response.getWriter().close();
+
+				// jsonp格式返回
+				/*String renderStr = "jsonpCallback" + "(" + jsonStr + ")";
+				response.setContentType("text/plain;charset=UTF-8");
+				response.getWriter().write(renderStr);*/
+
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 			return false;
-		}*/
+		}
 
-		return true;
 	}
 
 	/**
 	 * 在业务处理器处理请求执行完成后,生成视图之前执行的动作 可在modelAndView中加入数据，比如当前时间
 	 */
 	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
 		if (modelAndView != null) { // 加入当前时间
 			modelAndView.addObject("var", "测试postHandle");
 		}
@@ -72,7 +81,8 @@ public class AllInterceptor extends HandlerInterceptorAdapter {
 	 * 当有拦截器抛出异常时,会从当前拦截器往回执行所有的拦截器的afterCompletion()
 	 */
 	@Override
-	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
 	}
 
 }
